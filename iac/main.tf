25d44686a3b020acd953eaf4667a075867e5628b
ta93abe <ta93abe@ta93abe.com>
2025-10-16 23:39:07 +0900

    Format provider configuration for improved readability in main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -14,11 +14,12 @@ terraform {
 }
 
 provider "snowflake" {
-  organization_name = var.snowflake_organization_name
-  account_name      = var.snowflake_account_name
-  user              = var.snowflake_user
-  role              = var.snowflake_role
-  authenticator     = "SNOWFLAKE_JWT"
-  private_key       = var.snowflake_private_key
-  warehouse         = var.snowflake_warehouse
+  organization_name        = var.snowflake_organization_name
+  account_name             = var.snowflake_account_name
+  user                     = var.snowflake_user
+  role                     = var.snowflake_role
+  authenticator            = "SNOWFLAKE_JWT"
+  private_key              = var.snowflake_private_key
+  warehouse                = var.snowflake_warehouse
+  preview_features_enabled = true
 }
